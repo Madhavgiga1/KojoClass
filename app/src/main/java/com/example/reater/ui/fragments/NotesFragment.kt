7590fad3b37e5f57b60b202c21018af4390f25eb
Madhav <sharmamadhav208@gmail.com>
2024-03-31 02:15:03 +0530

    notes and assignment fragments

---
+++ app/src/main/java/com/example/reater/ui/fragments/NotesFragment.kt
@@ -0,0 +1,24 @@
+package com.example.reater.ui.fragments
+
+import android.os.Bundle
+import androidx.fragment.app.Fragment
+import android.view.LayoutInflater
+import android.view.View
+import android.view.ViewGroup
+import com.example.reater.R
+
+
+class NotesFragment : Fragment() {
+
+
+
+    override fun onCreateView(
+        inflater: LayoutInflater, container: ViewGroup?,
+        savedInstanceState: Bundle?
+    ): View? {
+        // Inflate the layout for this fragment
+        return inflater.inflate(R.layout.fragment_notes, container, false)
+    }
+
+
+}
