61d0f4008e73353abdc59bf521b3ca2482a7288c
Madhav <sharmamadhav208@gmail.com>
2024-04-07 20:00:22 +0530

    New layout for individual assignment and home fragment

--- app/src/main/java/com/example/reater/ui/fragments/HomeFragment.kt
+++ app/src/main/java/com/example/reater/ui/fragments/HomeFragment.kt
@@ -1,27 +1,29 @@
 package com.example.reater.ui.fragments
 
 import android.os.Bundle
-import androidx.fragment.app.Fragment
+import android.os.Handler
 import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
+import androidx.fragment.app.Fragment
 import com.example.reater.R
-
+import com.example.reater.databinding.FragmentHomeBinding
 
 class HomeFragment : Fragment() {
 
+    private var _binding: FragmentHomeBinding? = null
+    private val binding get() = _binding!!
 
-    override fun onCreate(savedInstanceState: Bundle?) {
-        super.onCreate(savedInstanceState)
-
-    }
 
     override fun onCreateView(
         inflater: LayoutInflater, container: ViewGroup?,
         savedInstanceState: Bundle?
     ): View? {
         // Inflate the layout for this fragment
-        return inflater.inflate(R.layout.fragment_home, container, false)
+        _binding = FragmentHomeBinding.inflate(inflater, container, false)
+        return binding.root
     }
+
+
 
 }
