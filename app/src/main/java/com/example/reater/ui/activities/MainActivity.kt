4b0bf8388e0077ca46afb55167aea51b68e19499
Madhav <sharmamadhav208@gmail.com>
2024-04-08 23:53:27 +0530

    enabled navigation

--- app/src/main/java/com/example/reater/ui/activities/MainActivity.kt
+++ app/src/main/java/com/example/reater/ui/activities/MainActivity.kt
@@ -2,11 +2,23 @@ package com.example.reater.ui.activities
 
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import androidx.navigation.NavController
+import androidx.navigation.fragment.NavHostFragment
+import androidx.navigation.ui.setupWithNavController
 import com.example.reater.R
+import com.google.android.material.bottomnavigation.BottomNavigationView
 
 class MainActivity : AppCompatActivity() {
+    private lateinit var navController: NavController
     override fun onCreate(savedInstanceState: Bundle?) {
+
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
+        val bottomNavigationView=findViewById<BottomNavigationView>(R.id.bottomNavigationView)
+        val navHostFragment =supportFragmentManager.findFragmentById(R.id.navHostFragment) as NavHostFragment
+        val navController:NavController=navHostFragment.navController
+
+
+        bottomNavigationView.setupWithNavController(navController)
     }
 }
