7bea7ea2100c04950ba2c4c3f01869a4ee67ac4e
Madhav <sharmamadhav208@gmail.com>
2024-04-09 23:57:43 +0530

    Network Module for di and Details Activity

---
+++ app/src/main/java/com/example/reater/di/NetworkModule.kt
@@ -0,0 +1,43 @@
+import dagger.Module
+import dagger.Provides
+import dagger.hilt.InstallIn
+import dagger.hilt.components.SingletonComponent
+import okhttp3.OkHttpClient
+import retrofit2.Retrofit
+import retrofit2.converter.gson.GsonConverterFactory
+import java.util.concurrent.TimeUnit
+import javax.inject.Singleton
+
+@Module
+@InstallIn(SingletonComponent::class)
+object NetworkModule {
+
+    @Singleton
+    @Provides
+    fun provideHttpClient() : OkHttpClient {
+        return OkHttpClient.Builder()
+            .readTimeout(15, TimeUnit.SECONDS)
+            .connectTimeout(15, TimeUnit.SECONDS)
+            .build()
+    }
+
+    @Singleton
+    @Provides
+    fun provideConverterFactory(): GsonConverterFactory {
+        return GsonConverterFactory.create()
+    }
+
+    @Singleton
+    @Provides
+    fun providesRetrofitInstance(
+        okHttpClient: OkHttpClient,
+        gsonConverterFactory: GsonConverterFactory
+    ):Retrofit{
+        return Retrofit.Builder()
+            .baseUrl("https://www.youtube.com/")
+            .client(okHttpClient)
+            .addConverterFactory(gsonConverterFactory).build()
+    }
+
+
+}
---
+++ app/src/main/java/com/example/reater/ui/activities/DetailsActivity.kt
@@ -0,0 +1,12 @@
+package com.example.reater.ui.activities
+
+import androidx.appcompat.app.AppCompatActivity
+import android.os.Bundle
+import com.example.reater.R
+
+class DetailsActivity : AppCompatActivity() {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_details)
+    }
+}
