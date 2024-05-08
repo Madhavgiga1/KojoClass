3f84a99044a3454da974b791c52cca64035089ad
Madhav <sharmamadhav208@gmail.com>
2024-05-08 23:59:40 +0530

    structure of teacher part of app

---
+++ app/src/main/java/com/example/reater/teacherapp/Teacher_Login_Activity.kt
@@ -0,0 +1,12 @@
+package com.example.reater.teacherapp
+
+import androidx.appcompat.app.AppCompatActivity
+import android.os.Bundle
+import com.example.reater.R
+
+class Teacher_Login_Activity : AppCompatActivity() {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_teacher_login)
+    }
+}
